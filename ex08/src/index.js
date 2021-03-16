e89b17b438935602394fa00b8db0bb022440180a
Spadalow <abdullatif.skejic@gmail.com>
2021-03-16 04:37:01 +0100

    true, false

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+var total = 0
+function funcWithArg(num){
+num=(6);
+ funcWithArg = (num+9)/5;
+ total = funcWithArg ;
+return (num+9)/5;
+
+
+}
+funcWithArg();
+
+console.log(total)
+module.exports = funcWithArg;
