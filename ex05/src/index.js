7fb2fbc113d6a73a7bdb262cf359613f3cb99bc7
Spadalow <abdullatif.skejic@gmail.com>
2021-03-16 07:16:18 +0100

    jeff bezos

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+var dress = "little dress";
+
+function myDress() {
+  // Only change code below this line
+
+ var dress = "ball gown"
+
+ // Only change code above this line
+  return dress;
+}
+
+myDress();
+console.log(dress)
+module.exports = myDress
